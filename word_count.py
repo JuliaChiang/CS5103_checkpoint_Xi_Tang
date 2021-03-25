81a794cadab5033231c645dc4318145a09acb81b
Xi Tang <2215007619@qq.com>
2021-03-25 11:18:56 +0800

    add word count functionality

---
+++ word_count.py
@@ -0,0 +1,36 @@
+f = open("lorem_ipsum.txt","r")
+test_string = f.read()
+
+
+capChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowChars = capChars.lower()
+chars = capChars + lowChars
+print(chars)
+
+seperators = [" ", "\t", "\n"]
+
+def is_word(short_seqs):
+    if short_seqs[-1] in seperators:
+        return short_seqs[:-1].isalpha()
+    else:
+        return False
+
+def word_split(text):
+    words = []
+    word = ''
+    if not text:
+        return words
+    else:
+        for ch in text:
+            if ch not in seperators:
+                word += ch
+            elif word:
+                words.append(word)
+                word = ''
+        return words
+
+def word_count(text):
+    words = word_split(text)
+    return len(words)
+
+print(word_count(test_string))
