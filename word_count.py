7f2de94899f7d60c89c5784dc22c4c1b0a4f1e02
Julia <52608969+JuliaChiang@users.noreply.github.com>
2021-03-25 11:55:54 +0800

    Add files via upload

---
+++ word_count.py
@@ -0,0 +1,52 @@
+f = open("lorem_ipsum.txt","r")
+test_string = f.read()
+
+
+capChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowChars = capChars.lower()
+chars = capChars + lowChars
+print(chars)
+
+seperators = [" ", "\t", "\n"]
+
+def is_word(short_seqs):
+    if short_seqs[-1] in seperators:
+        return short_seqs[:-1].isalpha()
+    else:
+        return False
+
+def word_split(text):
+    words = []
+    word = ''
+    if not text:
+        return words
+    else:
+        for ch in text:
+            if ch not in seperators:
+                word += ch
+            elif word:
+                words.append(word)
+                word = ''
+        return words
+
+def word_count(text):
+    words = word_split(text)
+    return len(words)
+
+def char_count(text):
+    count = 0
+    for ch in text:
+        if ch not in seperators:
+            count += 1
+    return count
+
+def line_count(text):
+    count = 0
+    for ch in text:
+        if ch == "\n":
+            count += 1
+    return count
+
+print(f"Words count: {word_count(test_string)}" )
+print(f"Characters count: {char_count(test_string)}" )
+print(f"Lines count: {line_count(test_string)}" )
