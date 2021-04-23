90594df5712e8019b63ab89009a573eb9f82ea44
Xi Tang <2215007619@qq.com>
2021-04-23 14:56:26 +0800

    add test for word count

--- unit_test.py
+++ unit_test.py
@@ -26,19 +26,31 @@ class TestWordMethods(unittest.TestCase):
         self.count = word_count.word_count("abcdefghijklmnopqrstuvwxyz")
         self.assertEqual(self.count, 1)
 
-    def test_count_one_word_with_tab(self):
+    def test_count_one_word_with_two_seperators(self):
+        self.count = word_count.word_count("\nabcdefghijklmnopqrstuvwxyz\t")
+        self.assertEqual(self.count, 1)
+
+    def test_count_one_word_with_sperator_at_the_end(self):
         self.count = word_count.word_count("abcdefghijklmnopqrstuvwxyz\t")
+        self.assertEqual(self.count, 1)
+
+    def test_count_one_word_with_sperator_at_the_start(self):
+        self.count = word_count.word_count("?abcdefghijklmnopqrstuvwxyz")
         self.assertEqual(self.count, 1)
 
     def test_count_two_words_with_space(self):
         self.count = word_count.word_count("abcdefghijklm nopqrstuvwxyz")
         self.assertEqual(self.count, 2)
 
+    def test_count_multiple_words_with_space(self):
+        self.count = word_count.word_count("ab cd ef\t gh \n mn, ls. ty? we; ok")
+        self.assertEqual(self.count, 9)
+
     def test_count_duplicates(self):
         self.count = word_count.word_count("abc abc")
         self.assertEqual(self.count, 1)
 
-class TestCharMethods(unittest.TestCase):
+'''class TestCharMethods(unittest.TestCase):
     def test_count_no_char(self):
         self.count = word_count.char_count("")
         self.assertEqual(self.count, 0)
@@ -84,7 +96,7 @@ class TestReplaceMethods(unittest.TestCase):
     def test_replace_two_words(self, mock_input):
         mock_input.side_effect = ['ab', 'xh']
         self.text = word_count.word_replace("ab cd ab")
-        self.assertEqual(self.text, "xh cd xh")
+        self.assertEqual(self.text, "xh cd xh")'''
 
 if __name__ == '__main__':
     unittest.main()
