3cd68f7d46754fb8e8613a424983c2eef3d95aa4
Julie17-XI <60849039+Julie17-XI@users.noreply.github.com>
2021-05-07 11:05:21 +0800

    add line_count and replacement to word-count.py

--- word_count.py
+++ word_count.py
@@ -1,36 +1,94 @@
-f = open("lorem_ipsum.txt","r")
-test_string = f.read()
+import config
+text = config.test_string
+chars = config.chars
+seperators = config.seperators
 
-
-capChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lowChars = capChars.lower()
-chars = capChars + lowChars
-print(chars)
-
-seperators = [" ", "\t", "\n"]
 
 def is_word(short_seqs):
-    if short_seqs[-1] in seperators:
-        return short_seqs[:-1].isalpha()
-    else:
+    if not short_seqs:
         return False
+    for i in short_seqs:
+        if i in seperators:
+            return False
+    return True
+
+def has_seperators(text):
+    for i in text:
+        if i in seperators:
+            return True
+    return False
+
+def find_sep_index(text):
+    length = len(text)
+    for i in range(length):
+        if text[i] in seperators:
+            return i
+    return None
+
+def has_new_line(text):
+    for i in text:
+        if i == "\n":
+            return True
+    return False
+
+def count_new_line(text):
+    count = 0
+    length = len(text)
+    for i in range(length):
+        if text[i] == "\n":
+            count += 1
+    return count
 
 def word_split(text):
-    words = []
-    word = ''
+    elements = []
+    if not text or text in seperators:
+        return elements
+
+    if is_word(text):
+        elements.append(text)
+        return elements
+    elif has_seperators(text):
+        sep_index = find_sep_index(text)
+        if sep_index == 0:
+            return word_split(text[1:])
+        if sep_index == len(text)-1:
+            return word_split(text[:-1])
+        else:
+            return word_split(text[:sep_index])+word_split(text[sep_index+1:])
+
+def text_break(text):
+    elements = []
     if not text:
-        return words
-    else:
-        for ch in text:
-            if ch not in seperators:
-                word += ch
-            elif word:
-                words.append(word)
-                word = ''
-        return words
+        return elements
+    elif is_word(text) or text in seperators:
+        elements.append(text)
+        return elements
+
+    elif has_seperators(text):
+        sep_index = find_sep_index(text)
+        front = text_break(text[:sep_index])
+        tail = text_break(text[sep_index+1:])
+        return front + [text[sep_index]] + tail
+
+def word_to_string(arr):
+    return ''.join(arr)
+
+def word_replace(text):
+    print(text)
+    target_word = input("Replace:")
+    new_word = input("Replace by:")
+    arr = text_break(text)
+    length = len(arr)
+    for i in range(length):
+        if arr[i].lower() == target_word.lower():
+            arr[i] = new_word
+    new_text = word_to_string(arr)
+    return new_text
+
 
 def word_count(text):
-    words = word_split(text)
+    words = word_split(text)git
+    words = set(words)
     return len(words)
 
 def char_count(text):
@@ -41,12 +99,11 @@ def char_count(text):
     return count
 
 def line_count(text):
-    count = 0
-    for ch in text:
-        if ch == "\n":
-            count += 1
-    return count
+    if not text:
+        return 0
+    if not has_new_line(text):
+        return 1
+    else:
+        count = count_new_line(text) + 1
+        return count
 
-print(f"Words count: {word_count(test_string)}" )
-print(f"Characters count: {char_count(test_string)}" )
-print(f"Lines count: {line_count(test_string)}" )
